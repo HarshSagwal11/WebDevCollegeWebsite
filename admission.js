c4cbfc3caa065b87f562506d736a032585b9ca2d
HarshSagwal11 <137023496+HarshSagwal11@users.noreply.github.com>
2023-06-19 10:31:06 +0530

    22

---
+++ admission.js
@@ -0,0 +1,34 @@
+// form validation
+$(document).ready(function () {
+    $("#contactform").submit(function (e) {
+      e.preventDefault();
+  
+      function validateForm() {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var subject = $("#subject").val();
+        var applied = $("#applied").val();
+        var message = $("#message").val();
+  
+        if (
+          name === "" ||
+          email === "" ||
+          mobile === "" ||
+          password1 === "" ||
+          password2 === ""
+        ) {
+          alert("Please fill in all fields");
+          return;
+        }
+  
+        $("#name").val("");
+        $("#Email").val("");
+        $("#subject").val("");
+        $("#applied").val("");
+        $("#message").val("");
+  
+        alert("SignedUp Successfully!");
+      }
+    });
+  });
+  
